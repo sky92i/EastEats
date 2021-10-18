d6bdfbf532bd565f69b20361ad87d1ab30200521
Agnes Hjeltnes <agneshjeltnes@Agness-MacBook-Pro.local>
2021-10-18 10:07:49 +0800

    Fix environment variables for DB authentication

--- app.py
+++ app.py
@@ -5,13 +5,12 @@ import json
 
 app = Flask(__name__)
 
-""" port = os.environ.get("MONGO_SERVER_PORT",9990)
+port = os.environ.get("MONGO_SERVER_PORT",9990)
 host = os.environ.get("MONGO_SERVER_HOST",9990)
 username = os.environ.get("MONGO_USERNAME",9990)
-password = os.environ.get("MONGO_PASSWORD",9990) """
+password = os.environ.get("MONGO_PASSWORD",9990)
 
-#client = MongoClient(username=username, port=int(port), host=host, password=password)
-client = MongoClient(username='comp3122', port=27017, host='mongo', password='12345')
+client = MongoClient(username=username, port=int(port), host=host, password=password)
 
 @app.route('/')
 def home():
