897905aec774f50873caf9e4e6aa0a9ec13294dd
polyu21022574x <21022574x@connect.polyu.hk>
2021-10-26 15:41:41 +0800

    update file structure

---
+++ app/app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import json
+
+app = Flask(__name__)
+
+port = os.environ.get("MONGO_SERVER_PORT",9990)
+host = os.environ.get("MONGO_SERVER_HOST",9990)
+username = os.environ.get("MONGO_USERNAME",9990)
+password = os.environ.get("MONGO_PASSWORD",9990)
+
+client = MongoClient(username=username, port=int(port), host=host, password=password)
+
+@app.route('/')
+def home():
+    return f"Welcome to EasyEats!"
+
+#start flask server
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=15000, debug=True)
