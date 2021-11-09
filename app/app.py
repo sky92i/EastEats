4ba8052221e18ae24bc6be46f31d3778a563fece
sky92i <48410483+sky92i@users.noreply.github.com>
2021-11-09 23:26:09 +0800

    Add error handling for KeyError and TypeError for order endpoints

--- app/app.py
+++ app/app.py
@@ -40,12 +40,17 @@ def accept_order(order_id):
     cursor = db.order.find({'order_id':order_id})
     if cursor.count() > 0:
         data = [remove_id(i) for i in cursor]
-        if request:
-            request_body = request.json
-            if not request_body["reason"]:
+        try:
+            if request:
+                request_body = request.json
+                if not request_body["reason"]:
+                    return jsonify({"error":"reason must be given"}), 400
+            else:
                 return jsonify({"error":"reason must be given"}), 400
-        else:
-            return jsonify({"error":"reason must be given"}), 400
+        except KeyError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
+        except TypeError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
 
         if data[0]['current_state'] == 'accepted':
             return jsonify({"error":"order is already accepted"}), 400
@@ -59,16 +64,21 @@ def deny_order(order_id):
     if cursor.count() > 0:
         data = [remove_id(i) for i in cursor]
         allowed_codes = ["store_closed","pos_not_ready","pos_offline","item_availability","missing_item","missing_info","pricing","capacity","address","special_instructions","other"]
-        if request:
-            request_body = request.json
-            if not request_body["reason"]:
+        try:
+            if request:
+                request_body = request.json
+                if not request_body["reason"]:
+                    return jsonify({"error":"reason must be given"}), 400
+                elif not (request_body["reason"]["explanation"] and request_body["reason"]["code"]):
+                    return jsonify({"error":"reason is not complete"}), 400
+                elif request_body["reason"]["code"] not in allowed_codes:
+                    return jsonify({"error":"reason code invalid"}), 400
+            else:
                 return jsonify({"error":"reason must be given"}), 400
-            elif not (request_body["reason"]["explanation"] and request_body["reason"]["code"]):
-                return jsonify({"error":"reason is not complete"}), 400
-            elif request_body["reason"]["code"] not in allowed_codes:
-                return jsonify({"error":"reason code invalid"}), 400
-        else:
-            return jsonify({"error":"reason must be given"}), 400
+        except KeyError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
+        except TypeError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
 
         if data[0]['current_state'] == 'denied':
             return jsonify({"error":"order is already denied"}), 400
@@ -84,26 +94,30 @@ def cancel_order(order_id):
         
         allowed_codes = ["out_of_items","kitchen_closed","customer_called_to_cancel","restaurant_too_busy","cannot_complete_customer_note","other"]
         allowed_parties = ["merchant", "customer"]
-
-        if request:
-            request_body = request.json
-            if not request_body["reason"]:
+        try:
+            if request:
+                request_body = request.json
+                if not request_body["reason"]:
+                    return jsonify({"error":"reason must be given"}), 400
+                elif not request_body["cancelling_party"]:
+                    return jsonify({"error":"cancelling party must be given"}), 400
+                elif request_body["reason"] not in allowed_codes:
+                    return jsonify({"error":"reason code invalid"}), 400
+                elif request_body["cancelling_party"] not in allowed_parties:
+                    return jsonify({"error":"invalid cancelling party"}), 400
+            else:
                 return jsonify({"error":"reason must be given"}), 400
-            elif not request_body["cancelling_party"]:
-                return jsonify({"error":"cancelling party must be given"}), 400
-            elif request_body["reason"] not in allowed_codes:
-                return jsonify({"error":"reason code invalid"}), 400
-            elif request_body["cancelling_party"] not in allowed_parties:
-                return jsonify({"error":"invalid cancelling party"}), 400
-        else:
-            return jsonify({"error":"reason must be given"}), 400
+        except KeyError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
+        except TypeError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
 
         if data[0]['current_state'] == 'canceled':
             return jsonify({"error":"order is already canceled"}), 400
         else:
             db.order.update_one({'order_id':order_id},{'$set':{'current_state':'canceled'}})
             send_webhook('cancel', order_id) # send webhook for canceled order
-            return jsonify({}), 200
+            return jsonify({}), 204
 
 @app.route('/orders/<order_id>/restaurantdelivery/status', methods=['POST'])
 def update_delivery_status(order_id):
@@ -111,11 +125,16 @@ def update_delivery_status(order_id):
     if cursor.count() > 0:
         request_body = request.json
         allowed_status = ["status","arriving","delivered"]
-        if request_body["status"] not in allowed_status:
-            return jsonify({"error":"Delivery status is not allowed"}), 400
-        else:
-            db.order.update_one({'order_id':order_id},{'$set':{'status':request_body["status"]}})
-            return jsonify({}), 204
+        try:
+            if request_body["status"] not in allowed_status:
+                return jsonify({"error":"Delivery status is not allowed"}), 400
+            else:
+                db.order.update_one({'order_id':order_id},{'$set':{'status':request_body["status"]}})
+                return jsonify({}), 204
+        except KeyError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
+        except TypeError:
+            return jsonify({"error":"the format of given data is incorrect"}), 400
 
 # for testing webhook when an order has been placed on the EasyEats
 @app.route('/testwebhook/addorder', methods={'GET'})
